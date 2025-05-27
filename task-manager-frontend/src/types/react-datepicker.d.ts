6d173ed8979757c57cccceb59c289c22591e210f
hmhngx <nhminhhung05@gmail.com>
2025-05-27 14:39:28 +0700

    chore: add react-datepicker type declaration

---
+++ task-manager-frontend/src/types/react-datepicker.d.ts
@@ -0,0 +1,16 @@
+declare module 'react-datepicker' {
+  import { Component } from 'react';
+
+  interface DatePickerProps {
+    selected: Date | null;
+    onChange: (date: Date | null) => void;
+    showTimeSelect?: boolean;
+    dateFormat?: string;
+    className?: string;
+    placeholderText?: string;
+    required?: boolean;
+  }
+
+  const DatePicker: React.FC<DatePickerProps>;
+  export default DatePicker;
+} 
