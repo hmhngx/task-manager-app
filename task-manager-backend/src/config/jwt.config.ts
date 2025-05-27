0098fca39bf897e4dc689dfc163e312c2d17e356
hmhngx <nhminhhung05@gmail.com>
2025-05-27 14:37:11 +0700

    fix: add fallback and clarify JWT config

--- task-manager-backend/src/config/jwt.config.ts
+++ task-manager-backend/src/config/jwt.config.ts
@@ -1,6 +1,8 @@
 import { JwtModuleOptions } from '@nestjs/jwt';
 
 export const getJwtConfig = (): JwtModuleOptions => ({
-  secret: process.env.JWT_SECRET,
-  signOptions: { expiresIn: process.env.JWT_EXPIRATION },
+  secret: process.env.JWT_SECRET || 'c82455ac-0068-4a90-9516-6bd234b556e2',
+  signOptions: { 
+    expiresIn: process.env.JWT_EXPIRATION || '24h',
+  },
 });
