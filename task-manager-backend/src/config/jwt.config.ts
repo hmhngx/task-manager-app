cf1643af9e081b97cb3b3541ed9ace17a67671a7
hmhngx <nhminhhung05@gmail.com>
2025-05-28 15:58:17 +0700

    chore: update JWT configuration

--- task-manager-backend/src/config/jwt.config.ts
+++ task-manager-backend/src/config/jwt.config.ts
@@ -2,7 +2,7 @@ import { JwtModuleOptions } from '@nestjs/jwt';
 
 export const getJwtConfig = (): JwtModuleOptions => ({
   secret: process.env.JWT_SECRET || 'c82455ac-0068-4a90-9516-6bd234b556e2',
-  signOptions: { 
+  signOptions: {
     expiresIn: process.env.JWT_EXPIRATION || '24h',
   },
 });
