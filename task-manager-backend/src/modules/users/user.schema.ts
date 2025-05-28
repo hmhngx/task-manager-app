f55aeed3df1e0105dbb52d77ae293974f2e75adc
hmhngx <nhminhhung05@gmail.com>
2025-05-28 16:02:15 +0700

    refactor: update user schema

--- task-manager-backend/src/modules/users/user.schema.ts
+++ task-manager-backend/src/modules/users/user.schema.ts
@@ -1,6 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
 @Schema()
 export class User extends Document {
   declare _id: Types.ObjectId;
@@ -10,6 +15,9 @@ export class User extends Document {
 
   @Prop({ required: true })
   password!: string;
+
+  @Prop({ type: String, enum: UserRole, default: UserRole.USER })
+  role!: UserRole;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
