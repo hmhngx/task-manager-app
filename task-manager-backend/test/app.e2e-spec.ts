cb224fb181183c93998e2d5bc6e9703db25a921c
hmhngx <nhminhhung05@gmail.com>
2025-05-31 23:59:03 +0700

    test: update e2e tests to match new auth flow

--- task-manager-backend/test/app.e2e-spec.ts
+++ task-manager-backend/test/app.e2e-spec.ts
@@ -16,10 +16,7 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
   });
 
   afterEach(async () => {
